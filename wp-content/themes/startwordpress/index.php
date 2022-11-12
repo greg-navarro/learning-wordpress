0447066516be92a7d995ebfa682a56b92bf50696
Gregory Litman-Navarro <gregorylitmannavarro@gmail.com>
2022-11-12 09:57:55 -0800

    adds single posts 'structure' and also adds comment spaces

--- wp-content/themes/startwordpress/index.php
+++ wp-content/themes/startwordpress/index.php
@@ -4,7 +4,16 @@
           <?php
           if ( have_posts() ) : while ( have_posts() ) : the_post();
             get_template_part( 'content', get_post_format() );
-          endwhile; endif;
+          endwhile; 
+          ?>
+          <nav>
+            <ul class="pager">
+              <li><?php next_posts_link( 'Previous' ); ?></li>
+              <li><?php previous_posts_link( 'Next' ); ?></li>
+          </ul>
+          </nav>
+          <?php
+          endif;
           ?>
         </div><!-- /.blog-main -->
         <?php 
